67cbd1f3a8c9b9da39dd397ec11603ac4d8f5785
Praise042 <praisechampion042@gmail.com>
2022-07-06 08:36:48 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,35 +1,39 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <udis86.h>
+
 /**
- * main - check the code for ALX School students.
- * @argc: amount of args
- * @argv: arguments
- * Return: 1 if an error occurred, 0 otherwise
- */
+  * main - ...
+  * @argc: ...
+  * @argv: ...
+  *
+  * Return: ...
+  */
 int main(int argc, char *argv[])
 {
-	unsigned char *p = (unsigned char *)main;
+	ud_t ud_obj;
+	int val = 0, i = 0;
 
-	unsigned int bytes;
-
-	int i;
-
-	if (argc != 2)
+	if (argc == 2)
 	{
-		printf("Error\n");
-		exit(1);
-	}
-	if (atoi(argv[1]) < 0)
-	{
-		printf("Error\n");
-		exit(2);
+		val = atoi(argv[1]);
+
+		if (val < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
+
+		ud_unit(&ud_obj);
+		ud_set_input_buffer(&ud_obj, argv[1], val);
+		ud_set_mode(&ud_obj, 64);
+		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
+
+		while (ud_disassemble(&ud_obj))
+		{
+			printf("\t%s\n", ud_insn_hex(&ud_obj));
+		}
 	}
 
-	bytes = atoi(argv[1]);
-
-	for (i = 0; i < bytes; i++)
-		printf("%02x ", *p++);
-	printf("\n");
 	return (0);
 }
